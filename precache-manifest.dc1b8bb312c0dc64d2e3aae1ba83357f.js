12b09ff81b0278623cd73877d14f889f88647daf
Sergio <sergio.sp@protonmail.com>
2021-05-09 13:31:32 -0500

    Updates

---
+++ precache-manifest.dc1b8bb312c0dc64d2e3aae1ba83357f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4d92e9a0e0911a1da9aeb25e19b0b6",
+    "url": "/almagua/index.html"
+  },
+  {
+    "revision": "041357f324aac29c5568",
+    "url": "/almagua/static/css/main.c512b6f5.chunk.css"
+  },
+  {
+    "revision": "d03d929c12f2bde8de45",
+    "url": "/almagua/static/js/2.f3a0b3b8.chunk.js"
+  },
+  {
+    "revision": "0788286610f00d1a8d8f763fc3087642",
+    "url": "/almagua/static/js/2.f3a0b3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041357f324aac29c5568",
+    "url": "/almagua/static/js/main.c1e4956d.chunk.js"
+  },
+  {
+    "revision": "af5256b26ebbdba09959",
+    "url": "/almagua/static/js/runtime-main.31cfc1c9.js"
+  },
+  {
+    "revision": "33b5a176848d601f5e15f573881a4cd8",
+    "url": "/almagua/static/media/approved-active.33b5a176.svg"
+  },
+  {
+    "revision": "49533e057e8f8b8c2eea717527d13c6c",
+    "url": "/almagua/static/media/approved.49533e05.svg"
+  },
+  {
+    "revision": "6c6a9b6a49243f0d4852d65cf9853f59",
+    "url": "/almagua/static/media/arrow-down-active.6c6a9b6a.svg"
+  },
+  {
+    "revision": "4d03e129692b1c6e80995fd81cf2bd8b",
+    "url": "/almagua/static/media/arrow-down.4d03e129.svg"
+  },
+  {
+    "revision": "c1922aec842358f0d28a541b234c49c5",
+    "url": "/almagua/static/media/arrow-up-active.c1922aec.svg"
+  },
+  {
+    "revision": "b19691ba7bed00927167a267a39a6d79",
+    "url": "/almagua/static/media/back.b19691ba.svg"
+  },
+  {
+    "revision": "46972929964f73c044d67160af855b94",
+    "url": "/almagua/static/media/calendar.46972929.svg"
+  },
+  {
+    "revision": "f96dcfd56290dcc05777574f0c87a544",
+    "url": "/almagua/static/media/ethereum-active.f96dcfd5.svg"
+  },
+  {
+    "revision": "9952940b57652477a2d8b8c5662c000e",
+    "url": "/almagua/static/media/ethereum.9952940b.svg"
+  },
+  {
+    "revision": "44f0a516f474d1b0726d2dd279b6270a",
+    "url": "/almagua/static/media/flag.44f0a516.svg"
+  },
+  {
+    "revision": "67d3eebf97a43b5faac3be5b60fb74a8",
+    "url": "/almagua/static/media/hand-active.67d3eebf.svg"
+  },
+  {
+    "revision": "6d3bd17e6713b0a4c0c6a1accb219f3a",
+    "url": "/almagua/static/media/hand.6d3bd17e.svg"
+  },
+  {
+    "revision": "e6f68c0326fefd19961599c727c9ec4a",
+    "url": "/almagua/static/media/logo-white.e6f68c03.png"
+  },
+  {
+    "revision": "e6f68c0326fefd19961599c727c9ec4a",
+    "url": "/almagua/static/media/logo.e6f68c03.png"
+  },
+  {
+    "revision": "eb8297d6084effb2ccd27992f683d65a",
+    "url": "/almagua/static/media/not-found.eb8297d6.svg"
+  },
+  {
+    "revision": "62e5a608ccc275f81b493dcc5382e5e2",
+    "url": "/almagua/static/media/open-active.62e5a608.svg"
+  },
+  {
+    "revision": "8b08adbb527fb04edb89f2c1f7ee3bed",
+    "url": "/almagua/static/media/open.8b08adbb.svg"
+  },
+  {
+    "revision": "982566ae47b305a323eb2025689ef238",
+    "url": "/almagua/static/media/paper-active.982566ae.svg"
+  },
+  {
+    "revision": "01d92b6e290ef6856b44f6d82109f400",
+    "url": "/almagua/static/media/paper.01d92b6e.svg"
+  },
+  {
+    "revision": "e3d983b4e5aabf479b530e76b4be6d3e",
+    "url": "/almagua/static/media/rejected-active.e3d983b4.svg"
+  },
+  {
+    "revision": "973d249df24c93273c73f406d4d1bcd3",
+    "url": "/almagua/static/media/rejected.973d249d.svg"
+  },
+  {
+    "revision": "22e775a3831c7728614c1c14468a8052",
+    "url": "/almagua/static/media/signout.22e775a3.svg"
+  },
+  {
+    "revision": "0b54f0cc55996b111c587c5a24018474",
+    "url": "/almagua/static/media/tweet-active.0b54f0cc.svg"
+  },
+  {
+    "revision": "e0e653eb13210dc222ef0888291b2c78",
+    "url": "/almagua/static/media/tweet.e0e653eb.svg"
+  },
+  {
+    "revision": "6b982f0fe73ccc7032a6a7c20be2d938",
+    "url": "/almagua/static/media/twitter-card.6b982f0f.png"
+  }
+]);
